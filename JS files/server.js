aba5e6499679183d56fa0c91d574176435e3ef96
Elijah Baptiste <elbaptis@usc.edu>
2025-06-09 16:23:20 -0700

    Created the npm project and installed the necessary dependencies. Didn't get the database connection or the page rendering working yet.

--- JS files/server.js
+++ JS files/server.js
@@ -1,12 +1,40 @@
 const mysql = require('mysql');
 const express = require('express');
+const app = express();
+const dotenv = require('dotenv').config();
+const path = require('path');
 
-// Using environment variables for database connection
-var connection = mysql.createConnection({
-    host: DATABASE_HOST,
-    user: DATABASE_USER,
-    password: DATABASE_PASSWORD,
-    database: 'FocusRoom'
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '..'));
+
+
+// // Using environment variables for database connection
+// var connection = mysql.createConnection({
+//     host:  process.env.DATABASE_HOST,
+//     user:  process.env.DATABASE_USER,
+//     password:  process.env.DATABASE_PASSWORD,
+//     database: 'FocusRoom'
+// });
+
+// connection.connect((err) => {
+//   if (err) throw err;
+//   console.log('Connected to the MySQL database!');
+// }); 
+
+
+app.get('/', (req, res) => {
+  res.render('WelcomPage.ejs')
 });
 
-connection.connect(); 
+
+
+//example prepared statement for querying the database
+// const sqlQuery = 'SELECT * FROM users WHERE id = ?';
+// connection.query(sqlQuery, [1], (error, results) => {
+//   if (error) throw error;
+//   console.log('The solution is: ', results[0].solution);
+// });
+ 
+// connection.end();
+
+app.listen(3000);
