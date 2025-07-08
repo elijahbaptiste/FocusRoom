f558702c033fc6c2fb14f06a872e326bf93da458
Elijah Baptiste <elbaptis@usc.edu>
2025-07-08 08:30:41 -0700

    So i changed the code to move the spotift PKCE code into the user dashboard ejs to keep the js file clean. It doesn't prompt people to log into spotify yet, so that needs to be added

--- JS files/server.js
+++ JS files/server.js
@@ -1,9 +1,17 @@
-const mysql = require('mysql2');
-const express = require('express');
+import mysql from 'mysql2';
+import express from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
+import bcrypt from 'bcrypt';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { EventEmitter } from 'events';
+EventEmitter.defaultMaxListeners = 20;  // Or higher if needed
+
+dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const app = express();
-const dotenv = require('dotenv').config();
-const path = require('path');
-const bcrypt = require('bcrypt');
 
 app.set('view engine', 'ejs');
 app.use('/Styles', express.static(path.join(__dirname, '../Styles')));
@@ -30,37 +38,104 @@ connection.connect((err) => {
 
 });
 
+//Prepared SQL queries
 const AddUser = 'INSERT INTO users (Username, password, email, SpotifyUserID) VALUES (?, ?, ?, ?)';
 const FindUser = 'SELECT * FROM users WHERE Username = ?';
 
-// Spotfiy API function setup. Code based on the Spotify API documentation found here : https://developer.spotify.com/documentation/web-api/tutorials/code-pkce-flow
-// const generateRandomString = (length) => {
-//   const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-//   const values = crypto.getRandomValues(new Uint8Array(length));
-//   return values.reduce((acc, x) => acc + possible[x % possible.length], "");
+// //Spotfiy API function setup. Code based on the Spotify API documentation found here : https://developer.spotify.com/documentation/web-api/tutorials/code-pkce-flow
+// const params = new URLSearchParams(window.location.search);
+// const code = params.get("code")
+
+// if (!code) {
+//   redirectToAuthCodeFlow(process.env.SPOTIFY_CLIENT_ID);
+// } else {
+//   const accessToken = await getAccessToken(process.env.SPOTIFY_CLIENT_ID, code);
+//   const profile = await fetchProfile(accessToken);
+//   console.log(profile);
+//   populateUI(profile);
 // }
 
-// const state  = generateRandomString(64);
+// export async function redirectToAuthCodeFlow(clientId) {
+//   // TODO: Redirect to Spotify authorization page
+//   const verifier = generateCodeVerifier(128);
+//   const challenge = await generateCodeChallenge(verifier);
 
-// const SpotfiyAuthorize = (req, res) => {
+//   localStorage.setItem("verifier", verifier);
 
-//   res.redirect('https://accounts.spotify.com/authorize?' +
-//     querystring.stringify({
-//       response_type: 'code',
-//       client_id: process.env.SPOTIFY_CLIENT_ID,
-//       scope: 'user-read-private user-read-email',
-//       redirect_uri: redirect_uri,
-//       state: state
-//     }));
-  
-//   return window.crypto.subtle.digest('SHA-256', data)
+//   const params = new URLSearchParams();
+//   params.append("client_id", process.env.SPOTIFY_CLIENT_ID);
+//   params.append("response_type", "code");
+//   params.append("redirect_uri", "http://127.0.0.1:3000/callback");
+//   params.append("scope", "user-read-private user-read-email");
+//   params.append("code_challenge_method", "S256");
+//   params.append("code_challenge", challenge);
+
+//   document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
 // }
 
+
+// function generateCodeVerifier(length) {
+//   let text = '';
+//   let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+//   for (let i = 0; i < length; i++) {
+//     text += possible.charAt(Math.floor(Math.random() * possible.length));
+//   }
+//   return text;
+// }
+
+// async function generateCodeChallenge(codeVerifier) {
+//   const data = new TextEncoder().encode(codeVerifier);
+//   const digest = await window.crypto.subtle.digest('SHA-256', data);
+//   return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
+//     .replace(/\+/g, '-')
+//     .replace(/\//g, '_')
+//     .replace(/=+$/, '');
+// }
+
+// async function getAccessToken(clientId, code) {
+//   // TODO: Get access token for code
+//   const verifier = localStorage.getItem("verifier");
+
+//   const params = new URLSearchParams();
+//   params.append("client_id", process.env.SPOTIFY_CLIENT_ID);
+//   params.append("grant_type", "authorization_code");
+//   params.append("code", code);
+//   params.append("redirect_uri", "http://127.0.0.1:3000/callback");
+//   params.append("code_verifier", verifier);
+
+//   const result = await fetch("https://accounts.spotify.com/api/token", {
+//     method: "POST",
+//     headers: { "Content-Type": "application/x-www-form-urlencoded" },
+//     body: params
+//   });
+
+//   const { access_token } = await result.json();
+//   return access_token;
+// }
+
+// async function fetchProfile(token) {
+//   // TODO: Call Web API
+//   const result = await fetch("https://api.spotify.com/v1/me", {
+//     method: "GET", headers: { Authorization: `Bearer ${token}` }
+//   });
+
+//   return await result.json();
+// }
+
+// function populateUI(profile) {
+//   // TODO: Update UI with profile data
+//   document.getElementById("Spotify Username").innerText = profile.display_name;
+//   if (profile.images[0]) {
+//     const profileImage = new Image(200, 200);
+//     profileImage.src = profile.images[0].url;
+//     document.getElementById("avatar").appendChild(profileImage);
+//   }
+// }
 
 
 app.get('/', (req, res) => {
   res.render('WelcomePage.ejs')
-
 });
 
 app.get('/login', (req, res) => {
@@ -72,12 +147,15 @@ app.get('/register', (req, res) => {
 });
 
 app.get('/dashboard', (req, res) => {
-  //SpotfiyAuthorize(req, res);
   res.render('UserDashboard.ejs')
 });
 
 app.get('/main', (req, res) => {
   res.render('MainListeningPage.ejs')
+});
+app.get('/callback', (req, res) => {
+  res.send('Callback reached');
+  res.render('UserDashboard.ejs');
 });
 
 app.post('/login', (req, res) => {
