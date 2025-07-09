1ba5bd624019c88073c2926c2616ac4b43c050b5
Elijah Baptiste <elbaptis@usc.edu>
2025-07-09 12:09:02 -0700

    tried to style some more but made it worst. I also started working on the listening page and now have the webcam video streaming into the website.

--- JS files/server.js
+++ JS files/server.js
@@ -15,6 +15,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.use('/Styles', express.static(path.join(__dirname, '../Styles')));
+app.use('/Assets', express.static(path.join(__dirname, '../Assets')));
 app.set('views', path.join(__dirname, '../Views'));
 app.use(express.urlencoded({ extended: false }));
 
@@ -41,98 +42,6 @@ connection.connect((err) => {
 //Prepared SQL queries
 const AddUser = 'INSERT INTO users (Username, password, email, SpotifyUserID) VALUES (?, ?, ?, ?)';
 const FindUser = 'SELECT * FROM users WHERE Username = ?';
-
-// //Spotfiy API function setup. Code based on the Spotify API documentation found here : https://developer.spotify.com/documentation/web-api/tutorials/code-pkce-flow
-// const params = new URLSearchParams(window.location.search);
-// const code = params.get("code")
-
-// if (!code) {
-//   redirectToAuthCodeFlow(process.env.SPOTIFY_CLIENT_ID);
-// } else {
-//   const accessToken = await getAccessToken(process.env.SPOTIFY_CLIENT_ID, code);
-//   const profile = await fetchProfile(accessToken);
-//   console.log(profile);
-//   populateUI(profile);
-// }
-
-// export async function redirectToAuthCodeFlow(clientId) {
-//   // TODO: Redirect to Spotify authorization page
-//   const verifier = generateCodeVerifier(128);
-//   const challenge = await generateCodeChallenge(verifier);
-
-//   localStorage.setItem("verifier", verifier);
-
-//   const params = new URLSearchParams();
-//   params.append("client_id", process.env.SPOTIFY_CLIENT_ID);
-//   params.append("response_type", "code");
-//   params.append("redirect_uri", "http://127.0.0.1:3000/callback");
-//   params.append("scope", "user-read-private user-read-email");
-//   params.append("code_challenge_method", "S256");
-//   params.append("code_challenge", challenge);
-
-//   document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
-// }
-
-
-// function generateCodeVerifier(length) {
-//   let text = '';
-//   let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-//   for (let i = 0; i < length; i++) {
-//     text += possible.charAt(Math.floor(Math.random() * possible.length));
-//   }
-//   return text;
-// }
-
-// async function generateCodeChallenge(codeVerifier) {
-//   const data = new TextEncoder().encode(codeVerifier);
-//   const digest = await window.crypto.subtle.digest('SHA-256', data);
-//   return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
-//     .replace(/\+/g, '-')
-//     .replace(/\//g, '_')
-//     .replace(/=+$/, '');
-// }
-
-// async function getAccessToken(clientId, code) {
-//   // TODO: Get access token for code
-//   const verifier = localStorage.getItem("verifier");
-
-//   const params = new URLSearchParams();
-//   params.append("client_id", process.env.SPOTIFY_CLIENT_ID);
-//   params.append("grant_type", "authorization_code");
-//   params.append("code", code);
-//   params.append("redirect_uri", "http://127.0.0.1:3000/callback");
-//   params.append("code_verifier", verifier);
-
-//   const result = await fetch("https://accounts.spotify.com/api/token", {
-//     method: "POST",
-//     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-//     body: params
-//   });
-
-//   const { access_token } = await result.json();
-//   return access_token;
-// }
-
-// async function fetchProfile(token) {
-//   // TODO: Call Web API
-//   const result = await fetch("https://api.spotify.com/v1/me", {
-//     method: "GET", headers: { Authorization: `Bearer ${token}` }
-//   });
-
-//   return await result.json();
-// }
-
-// function populateUI(profile) {
-//   // TODO: Update UI with profile data
-//   document.getElementById("Spotify Username").innerText = profile.display_name;
-//   if (profile.images[0]) {
-//     const profileImage = new Image(200, 200);
-//     profileImage.src = profile.images[0].url;
-//     document.getElementById("avatar").appendChild(profileImage);
-//   }
-// }
-
 
 app.get('/', (req, res) => {
   res.render('WelcomePage.ejs')
